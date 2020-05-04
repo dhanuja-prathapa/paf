895bc28d518ee3cc98704f2ed441d09ef4a378b9
dhanuja-prathapa <dhanujaprathapa@gmail.com>
2020-05-04 14:56:52 +0530

    idgenerate bug fix

--- HospitalManagement/src/main/java/com/project/healthcare/utils/idGenerate.java
+++ HospitalManagement/src/main/java/com/project/healthcare/utils/idGenerate.java
@@ -3,13 +3,17 @@ package com.project.healthcare.utils;
 import java.util.ArrayList;
 
 public class idGenerate {
-    public static int generateHosID(ArrayList<Integer> arrayList){
-        int id = arrayList.size();
-        id++;
-        if (arrayList.contains(id)){
+    public static int findnewid(ArrayList<Integer> arrayList,int id){
+        if(arrayList.contains(id) ){
             id++;
-        }
+           return findnewid(arrayList,id);
+        }else
         return id;
     }
-
+    public static int generateHosID(ArrayList<Integer> arrayList){
+        int id = arrayList.size();
+           id = findnewid(arrayList,id);
+        return id;
+    }
 }
+
